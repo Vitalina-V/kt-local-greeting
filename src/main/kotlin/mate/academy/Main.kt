bc52c50fbfff977750fe6f556b091c982b95c9cc
Volkivska <vitalina.v@air-dr.com>
2025-08-11 12:33:33 +0300

    fix

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,16 +1,16 @@
 package mate.academy
 
-fun greetUsers() {
-    println("Please enter a name (or press Enter to quit): ")
-    while (true) {
-        val name = readln()
-        if (name.isEmpty()) {
-            break
-        }
-        println("Hello, $name!")
-    }
-}
-
 fun main() {
+    fun greetUsers() {
+        println("Please enter a name (or press Enter to quit): ")
+        while (true) {
+            val name = readln()
+            if (name.isEmpty()) {
+                break
+            }
+            println("Hello, $name!")
+        }
+    }
+
     greetUsers()
 }
